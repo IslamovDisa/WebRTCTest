1a264363536eeff24faf6c01700954baf558847b
Islamov Denis <islamov.denis@gmail.com>
2019-12-10 04:54:54 +0300

    Add target highlight. Small fixes

---
+++ Assets/CustomWebRtcSenderReciever/Scripts/LocalPointClickInRectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class LocalPointClickInRectangle : MonoBehaviour
+{
+     private RectTransform _rectTransform => transform as RectTransform;
+     public event Action<Vector2> OnMouseClick;
+
+     [SerializeField] private TargetHighlight _targetHighlight;
+     
+     private void Update()
+     {
+         if (!Input.GetMouseButtonUp(0))
+         {
+             return;
+         }
+         
+         RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, Input.mousePosition,
+             GetComponentInParent<Canvas>().worldCamera, out var localPoint);
+         var normalizedPoint = Rect.PointToNormalized(_rectTransform.rect, localPoint);
+         OnMouseClick?.Invoke(normalizedPoint);
+         
+         _targetHighlight.SetTarget(normalizedPoint, _rectTransform.rect);
+         // Debug.Log(normalizedPoint);
+     }
+ }
---
+++ Assets/CustomWebRtcSenderReciever/Scripts/TargetHighlight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHighlight : MonoBehaviour
+{
+    [Space(10)]
+    [SerializeField] private RectTransform _highlightElement;
+    [SerializeField] private float _delay = 5.0f;
+    [SerializeField] private float _currentDelay;
+
+    private void Update()
+    {
+        if (!_highlightElement.gameObject.activeSelf)
+        {
+            return;
+        }
+        
+        if (_currentDelay < _delay)
+        {
+            _currentDelay += Time.deltaTime;
+        }
+        else
+        {
+            _highlightElement.gameObject.SetActive(false);
+            _currentDelay = 0.0f;
+        }
+    }
+
+    public void SetTarget(Vector2 position, Rect parent)
+    {
+        var point = Rect.NormalizedToPoint(parent, position);
+        _highlightElement.localPosition = point;
+        _currentDelay = 0.0f;
+        _highlightElement.gameObject.SetActive(true);
+    }
+}
